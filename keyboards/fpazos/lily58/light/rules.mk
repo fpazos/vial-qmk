0d4c7d54f777992816a03a1d35a99be2cab82bd0
Fernando Pazos Estévez <30479687+fpazos@users.noreply.github.com>
2024-06-29 13:10:34 +0200

    added other fpazos keyboards, updated lumberjack keymap

---
+++ keyboards/fpazos/lily58/light/rules.mk
@@ -0,0 +1,6 @@
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB light
+
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
---
+++ keyboards/fpazos/lily58l/rev1/rules.mk
@@ -0,0 +1,6 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
+
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
